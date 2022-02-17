5dd992e796ba188b1c1aead372cb795cfaa507a5
Provashish Roy <provashishroy95@gmail.com>
2022-02-17 19:58:17 +0600

    Code customization &  convert to function

--- js/script.js
+++ js/script.js
@@ -1,90 +1,105 @@
+//Getting input value dynamically..
 function gettingInputValue(id) {
     const inputElement = document.getElementById(id);
     const inputValue = parseFloat(inputElement.value);
     return inputValue;
 }
 
+//Insert inner text in HTML dynamically..
 function updateAmount(id, result) {
     document.getElementById(id).innerText = result.toFixed(2);
 }
 
-document.getElementById("calculate-btn").addEventListener("click", function() {
+//Error message showing function one..
+function gettingErrorTextOne(isTrue) {
+    const errorHandle = document.getElementById("notify-fail");
+    if (isTrue == true) {
+        errorHandle.style.display = "block";
+        errorHandle.innerText = "Your Input value is not valid";
+    } else {
+        errorHandle.style.display = "block";
+        errorHandle.innerText = "Your Expenses amount so high!";
+    }
+}
 
-    //Call function gettingInputValue() 
+//Error message showing and hide function two..
+function gettingErrorTextTwo(isTrue) {
+    const errorHandle = document.getElementById("notify-fail");
+    if (isTrue == true) {
+        errorHandle.style.display = "none";
+    } else {
+        errorHandle.style.display = "block";
+        errorHandle.innerText = "You have insufficien balance!";
+    }
+}
+
+
+//Income and expenses part start here...
+document.getElementById("calculate-btn").addEventListener("click", function() {
+    //Call function gettingInputValue()..
     const foodInputValue = gettingInputValue("food-input");
     const rentInputValue = gettingInputValue("rent-input");
     const clothesInputValue = gettingInputValue("clothes-input");
     const incomeInputValue = gettingInputValue("total-income");
-
-    //Check input value using error handler....
-    const errorHandle = document.getElementById("notify-fail");
+    //Check input value of income and expenses inputs..
     if (isNaN(foodInputValue) || isNaN(rentInputValue) || isNaN(clothesInputValue) || isNaN(incomeInputValue)) {
 
-        errorHandle.style.display = "block";
-        errorHandle.innerText = "Your Income or expenses value is not valid";
+        gettingErrorTextOne(true);
 
     } else if (foodInputValue < 0 || rentInputValue < 0 || clothesInputValue < 0 || incomeInputValue < 0) {
 
-        errorHandle.style.display = "block";
-        errorHandle.innerText = "Your Income or expenses Value is Negative!";
+        gettingErrorTextOne(true);
 
     } else {
-
-        //Calculate total Expenses and Balance
+        //Calculate total Expenses and Balance..
         const totalExpenses = foodInputValue + rentInputValue + clothesInputValue;
         const balance = incomeInputValue - totalExpenses;
-        //Compare expenses with income
+        //Compare expenses with income..
         if (totalExpenses > incomeInputValue) {
-            errorHandle.style.display = "block";
-            errorHandle.innerText = "Your Expenses amount so high!";
+
+            gettingErrorTextOne(false);
+
         } else {
-            errorHandle.style.display = "none";
-            //Call funtion updateAmount()
+            gettingErrorTextTwo(true);
+            //Call funtion updateAmount()..
             updateAmount("total-expenses", totalExpenses);
             updateAmount("balance", balance);
         }
     }
 });
 
-//savings part here
+//savings and remaining part start  here............
 document.getElementById("savings").addEventListener("click", function() {
-
-    //Call function gettingInputValue()
+    //Call function gettingInputValue()..
     const incomeInput = gettingInputValue("total-income");
     const savingInput = gettingInputValue("savings-input");
-
-    //check error savings and income input
-    const errorHandle = document.getElementById("notify-fail");
+    //check input value of savings and income..
     if (isNaN(savingInput) || isNaN(incomeInput)) {
 
-        errorHandle.style.display = "block";
-        errorHandle.innerText = "Your Saving or Income Input Value is invalid";
+        gettingErrorTextOne(true);
 
     } else if (savingInput < 0 || incomeInput < 0) {
 
-        errorHandle.style.display = "block";
-        errorHandle.innerText = "Your Saving or Income Input Value is Negative!";
+        gettingErrorTextOne(true);
 
     } else {
-
-        //getting Balance text value
+        //getting Balance text value..
         const balanceInput = document.getElementById("balance");
         const balanceAmount = parseFloat(balanceInput.innerText);
-
-        //Savings and remaining balance calculation here
+        //Savings and remaining balance calculation here..
         const savingValue = savingInput / 100;
         const savingAmount = incomeInput * savingValue;
         const remainingAmount = balanceAmount - savingAmount;
-        //Compare savings amount with balance amount
+        //Compare savings amount with balance amount..
         if (savingAmount > balanceAmount) {
-            errorHandle.style.display = "block";
-            errorHandle.innerText = "You have insuficient balance!";
+
+            gettingErrorTextTwo(false);
+
         } else {
-            errorHandle.style.display = "none";
-            //Call function updateAmount()
+            gettingErrorTextTwo(true);
+            //Call function updateAmount()..
             updateAmount("savings-amount", savingAmount);
             updateAmount("remaining-amount", remainingAmount);
         }
     }
-
 });
