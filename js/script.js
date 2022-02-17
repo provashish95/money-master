bcfe61fe79fafc81f20223085671201338ce3327
Provashish Roy <provashishroy95@gmail.com>
2022-02-17 18:48:59 +0600

    updated error handler

--- js/script.js
+++ js/script.js
@@ -16,52 +16,75 @@ document.getElementById("calculate-btn").addEventListener("click", function() {
     const clothesInputValue = gettingInputValue("clothes-input");
     const incomeInputValue = gettingInputValue("total-income");
 
-
     //Check input value using error handler....
     const errorHandle = document.getElementById("notify-fail");
     if (isNaN(foodInputValue) || isNaN(rentInputValue) || isNaN(clothesInputValue) || isNaN(incomeInputValue)) {
 
         errorHandle.style.display = "block";
-        errorHandle.innerText = "Please Enter number";
+        errorHandle.innerText = "Your Income or expenses value is not valid";
 
     } else if (foodInputValue < 0 || rentInputValue < 0 || clothesInputValue < 0 || incomeInputValue < 0) {
 
         errorHandle.style.display = "block";
-        errorHandle.innerText = "Your Input Value is Negative!";
+        errorHandle.innerText = "Your Income or expenses Value is Negative!";
 
     } else {
-        errorHandle.style.display = "none";
 
         //Calculate total Expenses and Balance
         const totalExpenses = foodInputValue + rentInputValue + clothesInputValue;
         const balance = incomeInputValue - totalExpenses;
-
-        //Call funtion updateAmount()
-        updateAmount("total-expenses", totalExpenses);
-        updateAmount("balance", balance);
+        //Compare expenses with income
+        if (totalExpenses > incomeInputValue) {
+            errorHandle.style.display = "block";
+            errorHandle.innerText = "Your Expenses amount so high!";
+        } else {
+            errorHandle.style.display = "none";
+            //Call funtion updateAmount()
+            updateAmount("total-expenses", totalExpenses);
+            updateAmount("balance", balance);
+        }
     }
 });
 
 //savings part here
 document.getElementById("savings").addEventListener("click", function() {
-    const incomeInput = document.getElementById("total-income");
-    const savingInput = document.getElementById("savings-input");
-    const balanceInput = document.getElementById("balance");
 
-    const balanceAmount = parseFloat(balanceInput.innerText);
+    //Call function gettingInputValue()
+    const incomeInput = gettingInputValue("total-income");
+    const savingInput = gettingInputValue("savings-input");
 
-    const incomeInputValue = parseFloat(incomeInput.value);
-    const savingInputValue = parseFloat(savingInput.value);
+    //check error savings and income input
+    const errorHandle = document.getElementById("notify-fail");
+    if (isNaN(savingInput) || isNaN(incomeInput)) {
 
-    const savingValue = savingInputValue / 100;
-    //console.log(savingValue.toFixed(2));
-    const savingAmount = incomeInputValue * savingValue;
-    // console.log(savingAmount.toFixed(2));
-    document.getElementById("savings-amount").innerText = savingAmount.toFixed(2);
+        errorHandle.style.display = "block";
+        errorHandle.innerText = "Your Saving or Income Input Value is invalid";
 
-    //remaining part here
-    const remainingAmount = balanceAmount - savingAmount;
-    document.getElementById("remaining-amount").innerText = remainingAmount.toFixed(2);
+    } else if (savingInput < 0 || incomeInput < 0) {
 
+        errorHandle.style.display = "block";
+        errorHandle.innerText = "Your Saving or Income Input Value is Negative!";
+
+    } else {
+
+        //getting Balance text value
+        const balanceInput = document.getElementById("balance");
+        const balanceAmount = parseFloat(balanceInput.innerText);
+
+        //Savings and remaining balance calculation here
+        const savingValue = savingInput / 100;
+        const savingAmount = incomeInput * savingValue;
+        const remainingAmount = balanceAmount - savingAmount;
+        //Compare savings amount with balance amount
+        if (savingAmount > balanceAmount) {
+            errorHandle.style.display = "block";
+            errorHandle.innerText = "You have insuficient balance!";
+        } else {
+            errorHandle.style.display = "none";
+            //Call function updateAmount()
+            updateAmount("savings-amount", savingAmount);
+            updateAmount("remaining-amount", remainingAmount);
+        }
+    }
 
 });
